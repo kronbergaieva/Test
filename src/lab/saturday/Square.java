5f9fe319f667858d1bc09e6b4207b2b2ff37a779
kronbergaieva <ieva_kronberga@inbox.lv>
2020-03-09 18:47:19 +0200

    first commit

---
+++ src/lab/saturday/Square.java
@@ -0,0 +1,22 @@
+package lab.saturday;
+/**
+ Calculates the area of a square given a side SaturdayLab1: Data types. Variables. Constants. Problem 2
+ Write a java program that declares a variable to represent the side of a square.
+ The side of the square is 7 units. Compute and display the area of the square.
+ Save the program as Square.java */
+//declare class called Square
+public class Square {
+	//declare main method to run the App
+public static void main(String[] args) {
+
+	//declare variables to store the side
+	
+			
+			int side;
+			//input
+			//assign/store the value 7 to variable named side
+			side = 7;
+			
+			//processing
+}	
+}	
